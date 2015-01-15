373c40103eae67fca467ad151ed8b144cbc446b5
WillianLuigi <willianluigii@hotmail.com>
2015-01-14 23:27:04 -0200

    ADDED INTERFACE COLOR OFFSETS/ADDRESSES

--- src/trainer.hpp
+++ src/trainer.hpp
@@ -10,7 +10,14 @@ public:
 	{
 		TRAINER_LIFE = 0xB793E0,
 		TRAINER_MONEY = 0xB7CE50, //money address
-		TRAINER_WANTED = 0xBAA420 //Wanted Level Adress
+		TRAINER_WANTED = 0xBAA420, //Wanted Level Adress
+		
+		//Interface address, which represents health bar/red text/enemy marker/anything red color
+		//initial Address = 0xBAB22C 4 bytes of RGBA.
+		TRAINER_RED = 0xBAB22C,			//red color, pointed on the initial address
+		TRAINER_BLUE = TRAINER_RED + 1,		//blue color, pointed on the second byte of the address
+		TRAINER_GREEN = TRAINER_RED + 2,	//green color, pointed on the third byte of the address
+		TRAINER_ALPHA = TRAINER_RED + 3		//alpha, pointed on the last byte of the address
 		/*
 		TRAINER_ARMOR = 0xB6F5F0 + 0x548, -> dynamic
 		TRAINER_X, -> dynamic
