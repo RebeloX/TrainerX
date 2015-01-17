c676983480a067dd9ae467bd1354b70707afd195
Carlos Rodrigues <kikito.rodrigues@gmail.com>
2015-01-17 02:13:23 +0000

    A little optimization

--- src/MemoryHandler.cpp
+++ src/MemoryHandler.cpp
@@ -3,22 +3,21 @@
 bool MemoryHandler::InitializeMemoryHandler()
 {
 	HWND hWnd = FindWindowA(0, "GTA:SA:MP"); //Finds the GTA:SA-MP window, you can change it to the GTA itself if you want to
+	
 	if (hWnd == 0){ //if the window isn't opened it sends an error
 		std::cerr << "Não foi possível encontrar o programa" << std::endl;
+		return false; // There isn't anything else to do.
 	}
-	else {
-		DWORD PID; //Stores the process id
-		GetWindowThreadProcessId(hWnd, &PID); //Gets the process ID
-		hProc = OpenProcess(PROCESS_ALL_ACCESS, false, PID); //Opens the process
-
-		if (!hProc) { //Cannot open the process
-			std::cerr << "Não foi possível abrir o processo." << std::endl;
-		}
-		else {
-			return true;
-		}
+	
+	DWORD PID; //Stores the process id
+	GetWindowThreadProcessId(hWnd, &PID); //Gets the process ID
+	hProc = OpenProcess(PROCESS_ALL_ACCESS, false, PID); //Opens the process
+	if (!hProc) { //Cannot open the process
+		std::cerr << "Não foi possível abrir o processo." << std::endl;
+		return false; // THere isn't anything to do.
+		
 	}
-	return false;
+	return true;
 }
 
 void MemoryHandler::CloseMemoryHandler()
